ddec8936ae4295dbdf6047133e474df0e1406aa4
DarovskikhMV <146919065+DarovskikhMV@users.noreply.github.com>
2024-01-31 12:00:46 +0300

    Update Dog.java

--- src/main/java/Clinic/Dog.java
+++ src/main/java/Clinic/Dog.java
@@ -1,2 +1,20 @@
-package Clinic;public class Dog {
+package Clinic;
+
+import java.time.LocalDate;
+
+public class Dog extends Animal implements Goable, Swimable {
+    public Dog(String name, LocalDate birthDate, String illness, String owner) {
+        super(name, birthDate, illness, owner);
+    }
+
+    @Override
+    public double run() {
+        return 15.0;
+    }
+
+
+    @Override
+    public double swim() {
+        return 10.0;
+    }
 }
