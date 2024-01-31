f0dd5f86769386df2c7d68158f0b97d45916b7ee
DarovskikhMV <146919065+DarovskikhMV@users.noreply.github.com>
2024-01-31 11:59:45 +0300

    Update Cat.java

--- src/main/java/Clinic/Cat.java
+++ src/main/java/Clinic/Cat.java
@@ -1,2 +1,18 @@
-package Clinic;public class Cat {
+package Clinic;
+
+import java.time.LocalDate;
+
+public class Cat extends Animal implements Goable {
+
+
+    public Cat(String name, LocalDate birthDate, String illness, String owner, int legsCount) {
+        super(name, birthDate, illness, owner);
+
+    }
+
+
+    @Override
+    public double run() {
+        return 9.0;
+    }
 }
