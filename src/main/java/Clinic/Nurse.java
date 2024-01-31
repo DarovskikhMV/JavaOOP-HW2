21460dac7f02e4dfe61381575e2328c5057a534d
DarovskikhMV <146919065+DarovskikhMV@users.noreply.github.com>
2024-01-31 12:03:00 +0300

    Update Nurse.java

--- src/main/java/Clinic/Nurse.java
+++ src/main/java/Clinic/Nurse.java
@@ -1,2 +1,35 @@
-package Clinic;public class Nurse {
+package Clinic;
+
+public class Nurse extends Doctor{
+    public Nurse(String name, String specialization, int experience) {
+        super(name, specialization, experience);
+    }
+    /**
+     Помогаем врачу с пациентом
+     */
+    public void assistDoctor(Doctor doctor, Animal patient) {
+        System.out.println("Nurse " + name + " is assisting Doctor " + doctor.name + " with patient " + patient.getName());
+    }
+
+    /**
+     Лечение пациента
+     */
+    public void treatment(Animal patient, String medication) {
+        System.out.println("Nurse " + name + " is treatment " + medication + " to patient " + patient.getName());
+    }
+
+    /**
+     Наблюдение за состоянием пациента
+     */
+    public void monitoring(Animal patient) {
+        System.out.println("Nurse " + name + " is monitoring of patient " + patient.getName());
+    }
+    @Override
+    public String toString() {
+        return "Nurses {" +
+                "Name ='" + name + '\'' +
+                ", Specialization =" + specialization +
+                ", EXP Years ='" + experience +
+                '}' + " \n";
+    }
 }
